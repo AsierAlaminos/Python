7ef2331370e6acd4e57a5524a2728076922874b9
AsierAlaminos <91246701+AsierAlaminos@users.noreply.github.com>
2022-02-21 08:40:40 +0100

    Add files via upload

--- calculadora.py
+++ calculadora.py
@@ -1,39 +1,80 @@
+from math import sqrt
+
 #PETICIÓN DE LA OPERACIÓN
-operacion=str(input("Dime la operación\n::: "))
+operacion=str(input("Dime la operación\t(Para ayuda escribe 'help')\n::: "))
 
 #SEPARADOR
-def separador(string):
-    global lista_separada
-    string=string+"·"
-    lista_separada=[]
+def separador_complementario(lista,string):
+    global stringv1
+    stringv1=string+"·"
     x=0
-    for i in range(len(string)):
-        if string[x] in "+-*/":
-            lista_separada.append(str(string[:x]))
-            lista_separada.append(str(string[x]))
-            string=string[x+1:]
+    for i in range(len(stringv1)):
+        if stringv1[0]=="(":
+            break
+        if stringv1[0] in "+-*/":
+            lista.append(str(stringv1[x]))
+            stringv1=stringv1[x+1:]
+        if stringv1[x] in "+-*/$":
+            if stringv1[x:x+2]=="**":
+                lista.append(str(stringv1[:x]))
+                lista.append(str(stringv1[x:x+2]))
+                stringv1=stringv1[x+2:]
+            else:
+                lista.append(str(stringv1[:x]))
+                lista.append(str(stringv1[x]))
+                stringv1=stringv1[x+1:]
             x=0
         x+=1
-        if string[x]=="·":
-            lista_separada.append(str(string[:x]))
+        if stringv1[x]=="·":
+            lista.append(str(stringv1[:x]))
             break
 
-#FILTRO
-def filtro(string, lista):
-    global numeritos, num_operacion
-    #DECLARACIÓN DE LAS LISTAS PARA EL ALGORITMO
-    numeritos=lista.copy()
-    num_operacion=lista.copy()
-
-    #ALGORITMO
-    for i in lista:
-        if i in "+-*/":
-            numeritos.remove(i)
+def separador(string):
+    global lista1
+    lista1=[]
+    lista_pasajera=[]
+    for u in range(len(string)):
+        if len(string)==0:
+            break
+        if string[-1]=="·":
+            string=string[:-1]
+        if string[0]=="(":
+            for i in range(len(string)):
+                if string[i]=="(":
+                    string=string[i:]
+                    for j in range(len(string)):
+                        if string[j]==")":                            
+                            string_pasajera=string[i+1:j]
+                            string=string[j+1:]
+                            separador_complementario(lista_pasajera,string_pasajera)
+                            if len(lista_pasajera)>=3:
+                                lista1.append(lista_pasajera)
+                                lista_pasajera=[]
+                            if len(string)==0:
+                                break
+                            elif string[0]=="(":
+                                break
+                            else:
+                                break
+                if len(string)<=0:
+                    break
+                            
+                if string[0]!="(":
+                    break
+        else:
+            separador_complementario(lista1,string)
+            string=stringv1
 
 #ORGANIZADOR
 def organizador(lista):
     global lista_organizada
     lista_organizada=[]
+    for i in range(len(lista)):
+        if lista[i]=="$":
+            lista_organizada.append(i)
+    for i in range(len(lista)):
+        if lista[i]=="**":
+            lista_organizada.append(i)
     for i in range(len(lista)):
         if lista[i]=="*" or lista[i]=="/":
             lista_organizada.append(i)
@@ -41,48 +82,75 @@ def organizador(lista):
         if lista[i]=="+" or lista[i]=="-":
             lista_organizada.append(i)
 
-separador(operacion)
-filtro(operacion, lista_separada)
-organizador(lista_separada)
-
-
 #OPERACIONES
-if len(lista_separada)<=2:
+def operaciones(lista,caracter):
+    if (lista[caracter])=="+":
+        res=float(lista[caracter-1])+float(lista[caracter+1])
+        lista.insert(caracter,res)
+        lista.pop(caracter-1)
+        lista.pop(caracter+1)
+        lista.pop(caracter)
+    elif lista[caracter]=="-":
+        res=float(lista[caracter-1])-float(lista[caracter+1])
+        lista.insert(caracter,res)
+        lista.pop(caracter-1)
+        lista.pop(caracter+1)
+        lista.pop(caracter)
+    elif lista[caracter]=="*":
+        res=float(lista[caracter-1])*float(lista[caracter+1])
+        lista.insert(caracter,res)
+        lista.pop(caracter-1)
+        lista.pop(caracter+1)
+        lista.pop(caracter)
+    elif lista[caracter]=="/":
+        if lista[0]!="0" or lista[1]!="0":
+            res=float(lista[caracter-1])/float(lista[caracter+1])
+            lista.insert(caracter,res)
+            lista.pop(caracter-1)
+            lista.pop(caracter+1)
+            lista.pop(caracter)
+        else:
+            print("En la división no puede haber un 0")
+    elif lista[caracter]=="**":
+        res=float(lista[caracter-1])**float(lista[caracter+1])
+        lista.insert(caracter,res)
+        lista.pop(caracter-1)
+        lista.pop(caracter+1)
+        lista.pop(caracter)
+    elif lista[caracter]=="$":
+        res=sqrt(float(lista[caracter+1]))
+        lista.insert(caracter,res)
+        lista.pop(caracter+1)
+        lista.pop(caracter)
+    else:
+        print("El operador debe estar entre estos: + - * /")
+    organizador(lista)
+
+separador(operacion)
+print(f"lista1 -> {lista1}")
+
+#BUCLES
+if operacion=="help":
+    print("suma=+'\nresta='-'\nmultiplicación='*'\ndivisión='/'\nexponentes='**'\nraiz cuadrada='$'")
+elif len(lista1)<=2:
     print("La operación está mal escrita\nEj: 2+5-5*9")
 else:
-    for i in range(len(numeritos)-1):
-        for i in lista_organizada:
-            if lista_separada[i]=="+":
-                res=float(lista_separada[i-1])+float(lista_separada[i+1])
-                lista_separada.insert(i,res)
-                lista_separada.pop(i-1)
-                lista_separada.pop(i+1)
-                lista_separada.pop(i)
-            elif lista_separada[i]=="-":
-                res=float(lista_separada[i-1])-float(lista_separada[i+1])
-                lista_separada.insert(i,res)
-                lista_separada.pop(i-1)
-                lista_separada.pop(i+1)
-                lista_separada.pop(i)
-            elif lista_separada[i]=="*":
-                res=float(lista_separada[i-1])*float(lista_separada[i+1])
-                lista_separada.insert(i,res)
-                lista_separada.pop(i-1)
-                lista_separada.pop(i+1)
-                lista_separada.pop(i)
-            elif lista_separada[i]=="/":
-                if lista_separada[0]!="0" or lista_separada[1]!="0":
-                    res=float(lista_separada[i-1])/float(lista_separada[i+1])
-                    lista_separada.insert(i,res)
-                    lista_separada.pop(i-1)
-                    lista_separada.pop(i+1)
-                    lista_separada.pop(i)
-                else:
-                    print("En la división no puede haber un 0")
-            else:
-                print("El operador debe estar entre estos: + - * /")
-            organizador(lista_separada)
+    organizador(lista1)
+    print(f"lista_organizada -> {lista_organizada}")
+    for i in range(len(lista1)):
+        if len(lista1[i])>=3:
+            organizador(lista1[i])
+            print(f"lista_organizada{lista_organizada}")
+            for u in lista_organizada:
+                operaciones(lista1[i],u)
+            lista1[i]=str(lista1[i][0])
+    for j in range(len(lista1)):
+        if len(lista1)<=1:
             break
+        else:
+            x=0
+            x+=1
+            operaciones(lista1,x)
                 
                 
-    print(f"El resultado es -> {lista_separada[0]}")
+    print(f"El resultado es -> {lista1[0]}")
--- pruebas.py
+++ pruebas.py
@@ -20,70 +20,165 @@ caracteres=operacion.split()
 print(len(caracteres))
 print(caracteres
 )
-"""
 
+
+def separador(lista,string):
+    global stringv1
+    stringv1=string+"·"
+    x=0
+    for i in range(len(stringv1)):
+        if stringv1[0]=="(":
+            break
+        if stringv1[0] in "+-*/":
+            lista.append(str(stringv1[x]))
+            stringv1=stringv1[x+1:]
+        if stringv1[x] in "+-*/":
+            lista.append(str(stringv1[:x]))
+            lista.append(str(stringv1[x]))
+            stringv1=stringv1[x+1:]
+            x=0
+        x+=1
+        if stringv1[x]=="·":
+            lista.append(str(stringv1[:x]))
+            break
+
+def separadorv2(string):
+    global lista1
+    lista1=[]
+    lista_pasajera=[]
+    for u in range(len(string)):
+        if len(string)==0:
+            break
+        if string[-1]=="·":
+            string=string[:-1]
+        if string[0]=="(":
+            for i in range(len(string)):
+                if string[i]=="(":
+                    string=string[i:]
+                    for j in range(len(string)):
+                        if string[j]==")":                            
+                            string_pasajera=string[i+1:j]
+                            string=string[j+1:]
+                            separador(lista_pasajera,string_pasajera)
+                            if len(lista_pasajera)>=3:
+                                lista1.append(lista_pasajera)
+                                lista_pasajera=[]
+                            break
+                if len(string)<=0:
+                    break
+                            
+                if string[0]!="(":
+                    break
+        else:
+            separador(lista1,string)
+            string=stringv1
+    print(f"lista1 -> {lista1}")
+
+def organizador(lista):
+    global lista_organizada
+    lista_organizada=[]
+    for i in range(len(lista)):
+        if lista[i]=="*" or lista[i]=="/":
+            lista_organizada.append(i)
+    for i in range(len(lista)):
+        if lista[i]=="+" or lista[i]=="-":
+            lista_organizada.append(i)
+    print(f"lista_organizada -> {lista_organizada}")
+
+separadorv2("(2+2*3)/3+(3+2*2)")
+organizador(lista1)
+2+2*3/3+3+2*2
+
+
+#PETICIÓN DE LA OPERACIÓN
+operacion=str(input("Dime la operación\n::: "))
+
+#SEPARADOR
 def separador(string):
+    global lista_separada
     string=string+"·"
     lista_separada=[]
     x=0
     for i in range(len(string)):
-        print("num x ->{}".format(x))
-        print("string ->{}".format(string[x]))
         if string[x] in "+-*/":
             lista_separada.append(str(string[:x]))
             lista_separada.append(str(string[x]))
             string=string[x+1:]
-            print(string)
             x=0
         x+=1
         if string[x]=="·":
             lista_separada.append(str(string[:x]))
             break
 
-def separadorv2(string):
-    lista1=[]
-    for u in range(len(string)):
-        if len(string)==0:
+#FILTRO
+def filtro(string, lista):
+    global numeritos, num_operacion
+    #DECLARACIÓN DE LA LISTA
+    numeritos=lista.copy()
+
+    #ALGORITMO
+    for i in lista:
+        if i in "+-*/":
+            numeritos.remove(i)
+
+#ORGANIZADOR
+def organizador(lista):
+    global lista_organizada
+    lista_organizada=[]
+    for i in range(len(lista)):
+        if lista[i]=="*" or lista[i]=="/":
+            lista_organizada.append(i)
+    for i in range(len(lista)):
+        if lista[i]=="+" or lista[i]=="-":
+            lista_organizada.append(i)
+
+separador(operacion)
+filtro(operacion, lista_separada)
+organizador(lista_separada)
+
+
+#OPERACIONES
+if len(lista_separada)<=2:
+    print("La operación está mal escrita\nEj: 2+5-5*9")
+else:
+    for i in range(len(numeritos)-1):
+        for i in lista_organizada:
+            if lista_separada[i]=="+":
+                res=float(lista_separada[i-1])+float(lista_separada[i+1])
+                lista_separada.insert(i,res)
+                lista_separada.pop(i-1)
+                lista_separada.pop(i+1)
+                lista_separada.pop(i)
+            elif lista_separada[i]=="-":
+                res=float(lista_separada[i-1])-float(lista_separada[i+1])
+                lista_separada.insert(i,res)
+                lista_separada.pop(i-1)
+                lista_separada.pop(i+1)
+                lista_separada.pop(i)
+            elif lista_separada[i]=="*":
+                res=float(lista_separada[i-1])*float(lista_separada[i+1])
+                lista_separada.insert(i,res)
+                lista_separada.pop(i-1)
+                lista_separada.pop(i+1)
+                lista_separada.pop(i)
+            elif lista_separada[i]=="/":
+                if lista_separada[0]!="0" or lista_separada[1]!="0":
+                    res=float(lista_separada[i-1])/float(lista_separada[i+1])
+                    lista_separada.insert(i,res)
+                    lista_separada.pop(i-1)
+                    lista_separada.pop(i+1)
+                    lista_separada.pop(i)
+                else:
+                    print("En la división no puede haber un 0")
+            else:
+                print("El operador debe estar entre estos: + - * /")
+            organizador(lista_separada)
             break
-        if string[-1]=="·":
-            string=string[:-1]
-            print(string)
-        if string[0]=="(":
-            for i in range(len(string)):
-                if string[i]=="(":
-                    for j in range(len(string)):
-                        if string[j]==")":
-                            lista1.append(string[i+1:j])
-                            string=string[j+1:]
-                            if len(string)==0:
-                                break
-                            elif string[0]=="(":
-                                break
-                            else:
-                                break
-                if string[0]!="(":
-                    break
-        else:
-            string=string+"·"
-            x=0
-            for i in range(len(string)):
-                if string[0]=="(":
-                    break
-                print("num x ->{}".format(x))
-                print("string ->{}".format(string[x]))
-                if string[0] in "+-*/":
-                    lista1.append(str(string[x]))
-                    string=string[x+1:]
-                    print(string)
-                if string[x] in "+-*/":
-                    lista1.append(str(string[:x]))
-                    lista1.append(str(string[x]))
-                    string=string[x+1:]
-                    print(string)
-                    x=0
-                x+=1
-                if string[x]=="·":
-                    lista1.append(str(string[:x]))
-                    break      
-    print(lista1)
-separadorv2("(2+2*3)/3+(3+2*2)")
+                
+                
+    print(f"El resultado es -> {lista_separada[0]}")
+"""
+string="(2+2*3)/3+(3+2**2)+9"
+print(string)
+print(string[1:3])
+print(string[3:])
